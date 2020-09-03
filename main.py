82e2051d285a0c376bb6552c2bacb4993f301e80
Leonardo <leoenav@gmail.com>
2020-09-03 14:43:40 -0300

    Add default and optional parameters

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 from fastapi import FastAPI
+from typing import Optional
+
+from enum import Enum
+
+from fastapi import FastAPI
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
+
+
+@app.get("/items/")
+async def read_item(
+    skip: int = 0, limit: int = 10
+):  # Url Query paraments /items/?skip=0&limit=10
+    return fake_items_db[skip : skip + limit]
 
 
 @app.get("/")
@@ -9,7 +30,7 @@ async def root():
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
+async def read_item(item_id: str, q: Optional[str] = None):
     return {"item_id": item_id}
 
 
@@ -21,3 +42,14 @@ async def read_user_me():
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
+
+
+@app.get("/model/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
